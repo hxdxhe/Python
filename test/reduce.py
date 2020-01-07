efd878c752bcb9c57d2edc7ce9702a8c4c330cd3
storm <5860034@qq.com>
2020-01-07 10:30:28 +0800

    基础

--- test/reduce.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time : 2019/12/29 10:55
-# @Author : LiDong
-# @Email : 5860034@qq.com
-# @File : reduce.py
-# @Project : test
-from functools import reduce
-filter()
-reduce
