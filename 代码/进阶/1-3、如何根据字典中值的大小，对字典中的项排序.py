d3e47b28ff9601a7f47519b000ad1aeec6d14591
storm <5860034@qq.com>
2020-01-13 10:08:17 +0800

    进阶

--- 代码/进阶/1-3、如何根据字典中值的大小，对字典中的项排序.py
+++ 代码/进阶/1-3、如何根据字典中值的大小，对字典中的项排序.py
@@ -3,12 +3,12 @@
 # @Email : 5860034@qq.com
 from random import randint
 a = {x: randint(60,100) for x in 'xyzabc'}
-# print(a)
+print(a)
 # 第一种方法
 # print(iter(a))
 # print(a.keys())
 b = zip(a.values(),a.keys())
-# b = zip(a.itervalues(),a.iterkeys())#在内存使用上更优化些
+# b = zip(a.itervalues(),a.iterkeys())#在内存使用上更优化些  在Python2中
 # print(list(b))
 c = sorted(list(b))
 print(c)
